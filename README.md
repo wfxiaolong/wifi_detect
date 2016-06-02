2d8f85185bd692277ea0cc248a782a75713adca4
linxiaolong <542136758@qq.com>
2016-06-02 10:11:34 +0800

    Chnage UI and add rich readme file...

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 #gelaisi_wifi is a objective-c project
+<hr>
+###The founction of the project:
+<hr>
+	
+* detect arp list(by the c code)
+* detect wifi status(information)
+* ping ip (for a loop to ping the wlan ip, use simplePing)
+* check up the device message buy the mac address(use the api from www.macvendorlookup.com, "thanks very much")
 
-##The founction of the project:
-
-1.detect arp list(by the c code)
-
-2.detect wifi status(information)
-
-3.ping ip (for a loop to ping the wlan ip, use simplePing)
-
-4.check up the device message buy the mac address(use the api from www.macvendorlookup.com, "thanks very much")
+###Demo Image:
+<hr>
+<img src="images/demo1.jpg" style="width:240px;">
+<img src="images/demo2.jpg" style="width:240px;">
 
 ##License
+<hr>
 
-kxsmb is open source and covered by a standard 2-clause BSD license. See the LICENSE file for more info.
+Gelaisi is open source and covered by a standard 2-clause BSD license. See the LICENSE file for more info.
 
-Samba is Free Software licensed under the GNU General Public License.
+Gelaisi is Free Software licensed under the GNU General Public License.
 
 ##Contact me
-
-tel:15913159968
-
-name:晓龙
+<hr>
+* tel:15913159968
+* email:542136758@qq.com
+* name:晓龙
--- gelaisi/AppDelegate.m
+++ gelaisi/AppDelegate.m
@@ -7,16 +7,9 @@
 //
 
 #import "AppDelegate.h"
-#import "WZDeviceInfo.h"
-#import "UMSocial.h"
-#import "MobClick.h"
 
-#import "UMSocialWechatHandler.h"
-#import "UMSocialQQHandler.h"
+#import "TMSearchController.h"
 
-#import "TMControllerManager.h"
-
-#define MobClickKey @"5594ff8167e58ee8f20006d9"
 
 @interface AppDelegate ()
 
@@ -28,8 +21,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 #ifndef DEBUG
-    [self mobTouch];
-    [self umTrack];
+    
 #endif
     
     int cacheSizeMemory = 4*1024*1024; // 4MB
@@ -39,7 +31,8 @@
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     [self.window makeKeyAndVisible];
-    self.window.rootViewController = [TMControllerManager shareMgr].rootController;
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:[[TMSearchController alloc] init]];
+    self.window.rootViewController = nav;
 
     return YES;
 }
@@ -64,36 +57,6 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
-#pragma mark - um click
-- (void)mobTouch {
-    /// 友盟数据分析
-    NSString *distributionKind = GetDistributionKind();
-    [MobClick startWithAppkey:MobClickKey reportPolicy:SEND_INTERVAL channelId:distributionKind];
-    NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
-    [MobClick setAppVersion:version];
-    
-    /// 友盟分享平台
-    [UMSocialData setAppKey:MobClickKey];
-    //设置微信AppId，设置分享url，默认使用友盟的网址
-    [UMSocialWechatHandler setWXAppId:@"wxf749f8afa615469c" appSecret:@"D5Vi98YW2SxtblWz" url:@"http://www.umeng.com/social"];
-    
-    //设置手机QQ的AppId，指定你的分享url，若传nil，将使用友盟的网址
-    [UMSocialQQHandler setQQWithAppId:@"1104631749" appKey:@"D5Vi98YW2SxtblWz" url:@"http://www.umeng.com/social"];
-    
-}
-
-#pragma mark -
-- (void)umTrack {
-    NSString * appKey = MobClickKey;
-    NSString * deviceName = [[[UIDevice currentDevice] name] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    NSString * mac = [WZDeviceInfo MAC_ADDR];
-    NSString * idfa = [WZDeviceInfo customIdfa];
-    NSString * idfv = [WZDeviceInfo idfv];
-    NSString * urlString = [NSString stringWithFormat:@"http://log.umtrack.com/ping/%@/?devicename=%@&mac=%@&idfa=%@&idfv=%@", appKey, deviceName, mac, idfa, idfv];
-    TMLOG(@"%@", urlString);
-    [NSURLConnection connectionWithRequest:[NSURLRequest requestWithURL: [NSURL URLWithString:urlString]] delegate:nil];
 }
 
 @end
